9e59b5d316e3a5c11a35de36d9f8cde95c5f4311
Jan-Marten de Boer <github@johmanx.com>
2020-10-06 18:47:52 +0200

    Use example as example, rather than long description

--- cmd/completion.go
+++ cmd/completion.go
@@ -8,7 +8,7 @@ import (
 var completionCmd = &cobra.Command{
 	Use:   "completion [bash|zsh|fish|powershell]",
 	Short: "Generate completion script",
-	Long: `To load completions:
+	Example: `To load completions:
 
 Bash:
 
